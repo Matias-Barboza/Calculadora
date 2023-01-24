64fabf824dcc83e1b0f6cfc2f77084eaf32404d5
Matias Barboza <matiasbarboza540@gmail.com>
2023-01-23 21:36:34 -0300

    Funcionalidad calculos

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -34,10 +34,15 @@ public class Calculadora extends JFrame{
     private JButton igualButton = new JButton("=");
     private JButton puntoButton = new JButton(".");
     /*
-    Campo de texto donde se reproduce el tecleo de la operacion y posteriormente su resultado
+    Label donde se reproduce el tecleo de la operacion y posteriormente su resultado
     --------------------------------------------------------------------------------------------------------------------
      */
     private JLabel pantalla = new JLabel();
+
+    private String[] expresion = new String[20];
+    private int indiceActual = 0;
+    private int cantidadEnPantalla = 0;
+    private String resultado = "";
 
     public Calculadora(){
         super("Calculadora simple con Java");
@@ -89,6 +94,10 @@ public class Calculadora extends JFrame{
         this.setResizable(false);
         this.setLayout(null);
 
+        /*
+        Añado los componentes al frame
+        ----------------------------------------------------------------------------------------------------------------
+         */
         this.add(ceroButton);
         this.add(unoButton);
         this.add(dosButton);
@@ -111,6 +120,7 @@ public class Calculadora extends JFrame{
         this.add(pantalla);
 
         inicializarAccionesDeBotones();
+        inicializarExpresion();
     }
 
     private void inicializarAccionesDeBotones(){
@@ -119,6 +129,15 @@ public class Calculadora extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                if(!resultado.equals("")){
+                    if(cantidadEnPantalla < 19){
+                        pantalla.setText("");
+                        pantalla.setText(pantalla.getText() + resultado);
+                        expresion[indiceActual] += resultado;
+                        cantidadEnPantalla = pantalla.getText().length();
+                        System.out.println(cantidadEnPantalla);
+                    }
+                }
             }
         });
 
@@ -126,10 +145,29 @@ public class Calculadora extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e) {
                 if(!pantalla.getText().isEmpty()) {
+                    /*
+                    Para la operacion de borrar un caracter, primero obtengo el String presente en el label pantalla y
+                    el caracter que va a ser eliminado, seteo el texto nuevamente en el label con un caracter menos con
+                    el metodo substring()
+                    ----------------------------------------------------------------------------------------------------
+                     */
                     String operacion = pantalla.getText();
+                    char borrado = operacion.charAt(operacion.length() - 1);
+
                     operacion = operacion.substring(0, operacion.length() - 1);
                     pantalla.setText(operacion);
+
+                    if(borradoEsOperacion(borrado)){
+                        expresion[indiceActual] = "";
+                        indiceActual -= 2;
+                    }else{
+                        expresion[indiceActual] = expresion[indiceActual].substring(0,expresion[indiceActual].length() - 1);
+                    }
+                    cantidadEnPantalla--;
+                }else{
+                    pantalla.setText("");
                 }
+                System.out.println(cantidadEnPantalla);
             }
         });
 
@@ -137,120 +175,406 @@ public class Calculadora extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e) {
                 pantalla.setText("");
+                inicializarExpresion();
+                indiceActual = 0;
+                cantidadEnPantalla = 0;
             }
         });
 
         sieteButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                pantalla.setText(pantalla.getText() + "7");
+
+                if(cantidadEnPantalla < 19) {
+                    pantalla.setText(pantalla.getText() + "7");
+                    expresion[indiceActual] += "7";
+                    cantidadEnPantalla++;
+                }else{
+                    JOptionPane.showMessageDialog(null,"La calculadora admite hasta 19 caracteres" +
+                            " en pantalla.","Aviso",JOptionPane.INFORMATION_MESSAGE);
+                }
+                System.out.println(cantidadEnPantalla);
             }
         });
 
         ochoButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                pantalla.setText(pantalla.getText() + "8");
+
+                if(cantidadEnPantalla < 19) {
+                    pantalla.setText(pantalla.getText() + "8");
+                    expresion[indiceActual] += "8";
+                    cantidadEnPantalla++;
+                }else{
+                    JOptionPane.showMessageDialog(null,"La calculadora admite hasta 19 caracteres" +
+                            " en pantalla.","Aviso",JOptionPane.INFORMATION_MESSAGE);
+                }
+                System.out.println(cantidadEnPantalla);
             }
         });
 
         nueveButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                pantalla.setText(pantalla.getText() + "9");
+
+                if(cantidadEnPantalla < 19) {
+                    pantalla.setText(pantalla.getText() + "9");
+                    expresion[indiceActual] += "9";
+                    cantidadEnPantalla++;
+                }else{
+                    JOptionPane.showMessageDialog(null,"La calculadora admite hasta 19 caracteres" +
+                            " en pantalla.","Aviso",JOptionPane.INFORMATION_MESSAGE);
+                }
+                System.out.println(cantidadEnPantalla);
             }
         });
 
         divisionButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                pantalla.setText(pantalla.getText() + "÷");
+
+                if(cantidadEnPantalla != 0) {
+                    pantalla.setText(pantalla.getText() + "÷");
+                    indiceActual++;
+                    expresion[indiceActual] = "÷";
+                    indiceActual++;
+                    cantidadEnPantalla++;
+                    System.out.println(cantidadEnPantalla);
+                }
             }
         });
 
         cuatroButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                pantalla.setText(pantalla.getText() + "4");
+
+                if(cantidadEnPantalla < 19) {
+                    pantalla.setText(pantalla.getText() + "4");
+                    expresion[indiceActual] += "4";
+                    cantidadEnPantalla++;
+                }else{
+                    JOptionPane.showMessageDialog(null,"La calculadora admite hasta 19 caracteres" +
+                            " en pantalla.","Aviso",JOptionPane.INFORMATION_MESSAGE);
+                }
+                System.out.println(cantidadEnPantalla);
             }
         });
 
         cincoButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                pantalla.setText(pantalla.getText() + "5");
+
+                if(cantidadEnPantalla < 19) {
+                    pantalla.setText(pantalla.getText() + "5");
+                    expresion[indiceActual] += "5";
+                    cantidadEnPantalla++;
+                }else{
+                    JOptionPane.showMessageDialog(null,"La calculadora admite hasta 19 caracteres" +
+                            " en pantalla.","Aviso",JOptionPane.INFORMATION_MESSAGE);
+                }
+                System.out.println(cantidadEnPantalla);
             }
         });
 
         seisButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                pantalla.setText(pantalla.getText() + "6");
+
+                if(cantidadEnPantalla < 19) {
+                    pantalla.setText(pantalla.getText() + "6");
+                    expresion[indiceActual] += "6";
+                    cantidadEnPantalla++;
+                }else{
+                    JOptionPane.showMessageDialog(null,"La calculadora admite hasta 19 caracteres" +
+                            " en pantalla.","Aviso",JOptionPane.INFORMATION_MESSAGE);
+                }
+                System.out.println(cantidadEnPantalla);
             }
         });
 
         multiplicacionButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                pantalla.setText(pantalla.getText() + "x");
+
+                if(cantidadEnPantalla != 0) {
+                    pantalla.setText(pantalla.getText() + "x");
+                    indiceActual++;
+                    expresion[indiceActual] = "x";
+                    indiceActual++;
+                    cantidadEnPantalla++;
+                    System.out.println(cantidadEnPantalla);
+                }
             }
         });
 
         unoButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                pantalla.setText(pantalla.getText() + "1");
+
+                if(cantidadEnPantalla < 19) {
+                    pantalla.setText(pantalla.getText() + "1");
+                    expresion[indiceActual] += "1";
+                    cantidadEnPantalla++;
+                }else{
+                    JOptionPane.showMessageDialog(null,"La calculadora admite hasta 19 caracteres" +
+                            " en pantalla.","Aviso",JOptionPane.INFORMATION_MESSAGE);
+                }
+                System.out.println(cantidadEnPantalla);
             }
         });
 
         dosButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                pantalla.setText(pantalla.getText() + "2");
+
+                if(cantidadEnPantalla < 19) {
+                    pantalla.setText(pantalla.getText() + "2");
+                    expresion[indiceActual] += "2";
+                    cantidadEnPantalla++;
+                }else{
+                    JOptionPane.showMessageDialog(null,"La calculadora admite hasta 19 caracteres" +
+                            " en pantalla.","Aviso",JOptionPane.INFORMATION_MESSAGE);
+                }
+                System.out.println(cantidadEnPantalla);
             }
         });
 
         tresButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                pantalla.setText(pantalla.getText() + "3");
+
+                if(cantidadEnPantalla < 19) {
+                    pantalla.setText(pantalla.getText() + "3");
+                    expresion[indiceActual] += "3";
+                    cantidadEnPantalla++;
+                }else{
+                    JOptionPane.showMessageDialog(null,"La calculadora admite hasta 19 caracteres" +
+                            " en pantalla.","Aviso",JOptionPane.INFORMATION_MESSAGE);
+                }
+                System.out.println(cantidadEnPantalla);
             }
         });
 
         restaButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+                if(cantidadEnPantalla != 0){
+                    indiceActual++;
+                    expresion[indiceActual] = "-";
+                    indiceActual++;
+                }else{
+                    expresion[indiceActual] += "-";
+                }
                 pantalla.setText(pantalla.getText() + "-");
+                cantidadEnPantalla++;
+                System.out.println(cantidadEnPantalla);
             }
         });
 
         ceroButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                pantalla.setText(pantalla.getText() + "0");
+
+                if(cantidadEnPantalla < 19) {
+                    pantalla.setText(pantalla.getText() + "0");
+                    expresion[indiceActual] += "0";
+                    cantidadEnPantalla++;
+                }else{
+                    JOptionPane.showMessageDialog(null,"La calculadora admite hasta 19 caracteres" +
+                            " en pantalla.","Aviso",JOptionPane.INFORMATION_MESSAGE);
+                }
+                System.out.println(cantidadEnPantalla);
             }
         });
 
         puntoButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                pantalla.setText(pantalla.getText() + ".");
+
+                if(cantidadEnPantalla < 19) {
+                    pantalla.setText(pantalla.getText() + ".");
+                    expresion[indiceActual] += ".";
+                    cantidadEnPantalla++;
+                }else{
+                    JOptionPane.showMessageDialog(null,"La calculadora admite hasta 19 caracteres" +
+                            " en pantalla.","Aviso",JOptionPane.INFORMATION_MESSAGE);
+                }
+                System.out.println(cantidadEnPantalla);
             }
         });
 
         igualButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                pantalla.setText(pantalla.getText() + "=");
+                pantalla.setText("");
+                indiceActual++;
+                imprimirExpresion();
+
+                realizarCalculo();
+                pantalla.setText(resultado);
+
+                inicializarExpresion();
+                indiceActual = 0;
             }
         });
 
         sumaButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+
+                if(cantidadEnPantalla != 0) {
+                    indiceActual++;
+                    expresion[indiceActual] = "+";
+                    indiceActual++;
+                }else{
+                    expresion[indiceActual] += "+";
+                }
                 pantalla.setText(pantalla.getText() + "+");
+                cantidadEnPantalla++;
+                System.out.println(cantidadEnPantalla);
             }
         });
 
+    }
+
+    private void inicializarExpresion(){
+        /*
+        Inicializo todas las posiciones del arreglo con "", para que no sean null, y por ende provoque errores
+        ----------------------------------------------------------------------------------------------------------------
+         */
+        for (int i = 0; i < expresion.length; i++) {
+            expresion[i] = "";
+        }
+    }
+
+    private void imprimirExpresion(){
+        for (int i = 0; i < indiceActual; i++){
+            System.out.print(expresion[i] + "[" + i + "]");
+        }
+        System.out.println();
+    }
+
+    private boolean borradoEsOperacion(char borrado){
+        return Character.compare(borrado,'÷') == 0 || Character.compare(borrado,'x') == 0
+                || Character.compare(borrado,'+') == 0 || Character.compare(borrado,'-') == 0;
+    }
+
+    private void realizarCalculo(){
+
+        int divisiones = 0, multiplicaciones = 0, sumas = 0, restas = 0;
+
+        for(int i = 0; i < indiceActual; i++){
+
+            if(expresion[i].equals("÷")){
+                divisiones++;
+            }else if(expresion[i].equals("x")){
+                multiplicaciones++;
+            }else if(expresion[i].equals("+")){
+                sumas++;
+            }else if(expresion[i].equals("-")){
+                restas++;
+            }
+        }
+
+        if(divisiones != 0){
+            int i = 0;
+            double resul;
+
+            while(divisiones != 0  && i < indiceActual){
+
+                if(expresion[i].equals("÷")){
+                    resul = Double.parseDouble(expresion[i-1]) / Double.parseDouble(expresion[i+1]);
+                    expresion[i-1] = String.valueOf(resul);
+                    reIndexarExpresion(i);
+                    divisiones--;
+                }else{
+                    i++;
+                }
+            }
+
+            imprimirExpresion();
+            System.out.println(indiceActual);
+            System.out.println(i);
+        }
+
+        if(multiplicaciones != 0){
+            int i = 0;
+            double resul;
+
+            while(multiplicaciones != 0 && i < indiceActual){
+
+                if(expresion[i].equals("x")){
+                    resul = Double.parseDouble(expresion[i-1]) * Double.parseDouble(expresion[i+1]);
+                    expresion[i-1] = String.valueOf(resul);
+                    reIndexarExpresion(i);
+                    multiplicaciones--;
+                }else{
+                    i++;
+                }
+            }
+
+            imprimirExpresion();
+            System.out.println(indiceActual);
+            System.out.println(i);
+        }
+
+        if(restas != 0){
+            int i = 0;
+            double resul;
+
+            while(restas != 0 && i < indiceActual){
+
+                if(expresion[i].equals("-")){
+                    resul = Double.parseDouble(expresion[i-1]) - Double.parseDouble(expresion[i+1]);
+                    expresion[i-1] = String.valueOf(resul);
+                    reIndexarExpresion(i);
+                    restas--;
+                }else{
+                    i++;
+                }
+            }
+
+            imprimirExpresion();
+            System.out.println(indiceActual);
+            System.out.println(i);
+        }
+
+        if(sumas != 0){
+            int i = 0;
+            double resul;
+
+            while(sumas != 0 && i < indiceActual){
+
+                if(expresion[i].equals("+")){
+                    resul = Double.parseDouble(expresion[i-1]) + Double.parseDouble(expresion[i+1]);
+                    expresion[i-1] = String.valueOf(resul);
+                    reIndexarExpresion(i);
+                    sumas--;
+                }else{
+                    i++;
+                }
+            }
+
+            imprimirExpresion();
+            System.out.println(indiceActual);
+            System.out.println(i);
+        }
+
+        if(Double.parseDouble(expresion[0]) % 1 == 0.0){
+            int result = (int) Double.parseDouble(expresion[0]);
+            resultado = String.valueOf(result);
+        }else {
+            resultado = expresion[0];
+        }
+    }
+
+    private void reIndexarExpresion(int indiceInicio){
+
+        for(int i = indiceInicio+2; i < indiceActual; i++){
+            expresion[i-2] = expresion[i];
+        }
+
+        indiceActual -= 2;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         //En construccion...
-        Calculadora calcu = new Calculadora();
+        Calculadora calculadoraSimple = new Calculadora();
     }
 }
